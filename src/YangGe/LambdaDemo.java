7bd1cea7f4bc2143e9c993f81657aa684d90d9a5
jh0904 <464062919@qq.com>
2018-08-23 14:50:38 +0800

    复习简单的函数式编程

--- src/YangGe/LambdaDemo.java
+++ src/YangGe/LambdaDemo.java
@@ -9,27 +9,40 @@ package YangGe;
  * 1. 拷贝中括号+写死右箭头+落地大括号
  * 2. 一个接口有且仅有一个方法(默认就是函数式接口)
  * 3. @FunctionalInterface
+ * 4.
  */
 @FunctionalInterface
 interface Foo{
-	public void sayHello();
+	//public void sayHello();
 	//public void show();
+	public int add(int x,int y);
+
+	default int div(int x,int y){
+		return x/y;
+	}
+	public static int sub(int x,int y){
+		return x-y;
+	}
+
 }
 
 
 public class LambdaDemo {
 	public static void main(String[] args) {
-		Foo foo = new Foo () {
+		/*Foo foo = new Foo () {
 			@Override
 			public void sayHello() {
 				System.out.println ("-----hello---->");
 			}
 
-		};
+		};*/
 
-		Foo foo1=()->{
-			System.out.println ("hello---->");
-		};
+		Foo foo1=(x,y)-> x+y;
+		int add = foo1.add (0, 4);
+		System.out.println (add);
 
+		int div = foo1.div (6, 3);
+		System.out.println (div);
+		System.out.println (foo1.add (1, 4));
 	}
 }
