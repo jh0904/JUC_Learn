cb705346db085eae1ddff42c2a81c83a0a633211
jh0904 <464062919@qq.com>
2018-08-23 16:19:41 +0800

    实现Lock版本，记住两个三角形。实现4个线程，两加两减，线程的判断使用while

--- src/YangGe/NotifyWaitDemo.java
+++ src/YangGe/NotifyWaitDemo.java
@@ -1,11 +1,15 @@
 package YangGe;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * YangGe
  *
  * @author jh
  * @date 2018/8/23 14:50
- * description:
+ * description:乒乓球（生产者/消费者）
  * 现在两个线程，
  * 可以操作初始值为零的一个变量，
  * 实现一个线程对该变量加1，一个线程对该变量减1，
@@ -19,12 +23,51 @@ package YangGe;
  * 2.1  判断
  * 2.2  干活
  * 2.3  通知
- * 多线程判断用while（就像过安检）
+ * 多线程判断用while（就像过安检）防止虚假唤醒
  */
 class ShareData {
 	private int number = 0;
+	private Lock lock = new ReentrantLock ();
+	private Condition condition=lock.newCondition ();
 
-	public synchronized void increment() throws InterruptedException {
+	public void increment() throws InterruptedException {
+		lock.lock ();
+		try {
+			while (number != 0) {
+				condition.await ();
+			}
+			//干活
+			number++;
+			System.out.println (Thread.currentThread ().getName () + "\t" + number);
+			//通知
+			condition.signalAll ();
+		} catch (Exception e) {
+			e.printStackTrace ();
+		} finally {
+			lock.unlock ();
+		}
+
+	}
+	public void decrement() throws InterruptedException {
+		lock.lock ();
+		try {
+			while (number == 0) {
+				condition.await ();
+			}
+			//干活
+			number--;
+			System.out.println (Thread.currentThread ().getName () + "\t" + number);
+			//通知
+			condition.signalAll ();
+		} catch (Exception e) {
+			e.printStackTrace ();
+		} finally {
+			lock.unlock ();
+		}
+
+	}
+
+	/*public synchronized void increment() throws InterruptedException {
 		//判断
 		while (number!=0){
 		//if (number != 0) {
@@ -48,64 +91,56 @@ class ShareData {
 		System.out.println (Thread.currentThread ().getName () + "\t" + number);
 		//通知
 		this.notifyAll ();
-	}
+	}*/
 
 }
 
 public class NotifyWaitDemo {
 	public static void main(String[] args) {
-		ShareData sd = new ShareData();
-		new Thread(() -> {
-			for (int i = 1; i <=10; i++)
-			{
-				try
-				{
-					Thread.sleep(200);
-					sd.increment();
+		ShareData sd = new ShareData ();
+		new Thread (() -> {
+			for (int i = 1; i <= 10; i++) {
+				try {
+					Thread.sleep (200);
+					sd.increment ();
 				} catch (InterruptedException e) {
-					e.printStackTrace();
+					e.printStackTrace ();
 				}
 			}
-		}, "AA").start();
+		}, "AA").start ();
 
-		new Thread(() -> {
-			for (int i = 1; i <=10; i++)
-			{
-				try
-				{
-					Thread.sleep(300);
-					sd.decrement();
+		new Thread (() -> {
+			for (int i = 1; i <= 10; i++) {
+				try {
+					Thread.sleep (300);
+					sd.decrement ();
 				} catch (InterruptedException e) {
-					e.printStackTrace();
+					e.printStackTrace ();
 				}
 			}
-		}, "BB").start();
+		}, "BB").start ();
 
-		new Thread(() -> {
-			for (int i = 1; i <=10; i++)
-			{
-				try
-				{
-					Thread.sleep(400);
-					sd.increment();
+		new Thread (() -> {
+			for (int i = 1; i <= 10; i++) {
+				try {
+					Thread.sleep (400);
+					sd.increment ();
 				} catch (InterruptedException e) {
-					e.printStackTrace();
+					e.printStackTrace ();
 				}
 			}
-		}, "CC").start();
+		}, "CC").start ();
 
-		new Thread(() -> {
-			for (int i = 1; i <=10; i++)
-			{
-				try
-				{
-					Thread.sleep(500);
-					sd.decrement();
+		new Thread (() -> {
+			for (int i = 1; i <= 10; i++) {
+				try {
+					Thread.sleep (500);
+					sd.decrement ();
 				} catch (InterruptedException e) {
-					e.printStackTrace();
+					e.printStackTrace ();
 				}
 			}
-		}, "DD").start();
+		}, "DD").start ();
 
 	}
 }
